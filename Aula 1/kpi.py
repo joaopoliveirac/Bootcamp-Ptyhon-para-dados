e4f65db1d57dcc5ba9ab4f2e1995405b284a65e7
Joao Pedro <joaopo.0607@gmail.com>
2024-12-06 10:40:18 -0300

    desafio

--- Aula 1/kpi.py
+++ Aula 1/kpi.py
@@ -1,1 +1,12 @@
-print('teste')
+nome = input('digite seu nome')
+salario = float(input('informe seu salario'))
+constante_bonus = 1000
+bonus = float(input('informe o bonus recebido em %'))
+valor_bonus = constante_bonus + salario * bonus
+print(valor_bonus)
+
+
+
+
+
+
