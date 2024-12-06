d802dcf493940c3b2d9e06f8e8d11551748854f1
Joao Pedro <joaopo.0607@gmail.com>
2024-12-06 14:38:02 -0300

    exercicios string

--- Aula 2/exercicios.py
+++ Aula 2/exercicios.py
@@ -61,10 +61,25 @@
 # #### Strings (`str`)
 
 # 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+# texto = input('informe uma palavra ou frase').upper()
+# print(texto)
+
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+# nome = input('informe o nome do usuario').lower()
+# print(nome)
+
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
+# frase = input('informe uma frase.')
+# print(frase.strip())
+
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+# data = input('informe a data no formato dd/mm/aaaa').split('/')
+# print(data)
+
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
+# texto_01 = input('informe a primeira frase')
+# texto_02 = input('informe a segunda frase')
+# print(texto_01 + texto_02)
 
 # #### Booleanos (`bool`)
 
