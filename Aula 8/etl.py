c6f69c246a5f0f724256b15afccc813988224b85
Joao Pedro <joaopo.0607@gmail.com>
2024-12-15 00:15:09 -0300

    Construindo as funções e fazendo o teste com o usuario final na pipeline

--- Aula 8/etl.py
+++ Aula 8/etl.py
@@ -1,1 +1,31 @@
-pip install pandera
+import pandas as pd
+import os
+import glob
+
+#funcao de extract que le e consolidade os json
+
+def extrair_dados_e_consolidar(pasta: str) -> pd.DataFrame:
+    arquivos_json = glob.glob(os.path.join(pasta, '*.json')) 
+    df_list = [pd.read_json(arquivo) for arquivo in arquivos_json]  
+    df_total = pd.concat(df_list, ignore_index=True) 
+    return df_total
+
+#funcao que transforma
+def calcular_kpi_de_total_de_vendas(df: pd.DataFrame) -> pd.DataFrame:
+    df['Total'] = df['Quantidade'] * df['Venda']
+    return df
+
+#funcao que da load em csv ou parque
+
+def carregar_dados(df: pd.DataFrame, format_saida: list):
+    for formato in format_saida:
+        if formato == 'csv':
+            df.to_csv('dados.csv', index=False)
+        if formato == 'parquet':
+            df.to_parquet('dados.parquet', index=False)
+
+
+def pipeline_calcular_kpi_de_vendas_consolidado(pasta,format_saida):
+    data_frame = extrair_dados_e_consolidar(pasta)
+    data_frame_calculado = calcular_kpi_de_total_de_vendas(data_frame)
+    carregar_dados(data_frame_calculado,format_saida)
---
+++ Aula 8/pipeline.py
@@ -0,0 +1,6 @@
+from etl import pipeline_calcular_kpi_de_vendas_consolidado
+
+pasta = 'data'
+format_saida = ['csv']
+
+pipeline_calcular_kpi_de_vendas_consolidado(pasta,format_saida)
